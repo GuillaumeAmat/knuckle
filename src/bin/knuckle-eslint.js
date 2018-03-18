ad9e32da193f99b5f82977eb6bfdd4761a8ffd02
François Rosato <francois.rosato@ekino.com>
2018-03-18 11:35:52 +0100

    feat: eslint working script

--- src/bin/knuckle-eslint.js
+++ src/bin/knuckle-eslint.js
@@ -1,15 +1,28 @@
 import spawn from "cross-spawn";
+import path from "path";
 import program from "commander";
 
 import getClientWorkingDir from "./utils/getClientWorkingDir";
+import getPathToBin from "./utils/getPathToBin";
+import getLocalConfigurationFile from "./utils/getLocalConfigurationFile";
 
 program
   .option("-f, --fix", "fix files")
-  .action(() => {
+  .action(filesPath => {
     const workingDirectory = getClientWorkingDir();
-    console.log("workingDirectory", workingDirectory);
 
-    const result = spawn.sync("echo", "@TODO eslint");
+    const eslintDefaultConfig = getLocalConfigurationFile("eslintrc.js");
+    const targetPath = path.join(workingDirectory, filesPath);
+
+    const commandArgs = [targetPath, "--config", eslintDefaultConfig];
+
+    if (program.fix) {
+      commandArgs.push("--fix");
+    }
+
+    const eslint = getPathToBin("eslint");
+
+    const result = spawn.sync(eslint, [...commandArgs], { stdio: "inherit" });
 
     process.exit(result.status);
   })
