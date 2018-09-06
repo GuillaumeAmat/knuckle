4b03f1ea4bd4577f136dd0b40214fd950fdcd482
Guillaume AMAT <guillaume@amat.io>
2018-09-07 00:58:55 +0200

    chore(tslint): use formatJson for the TSLint config

--- src/tools/tslint/configurations.js
+++ src/tools/tslint/configurations.js
@@ -1,9 +1,10 @@
+const { formatJson } = require('../../utils/file');
 const loadAndMergeConfig = require('../../utils/loadAndMergeConfig');
 
 module.exports = [
   {
     filename: 'tslint.json',
     get: () => loadAndMergeConfig('tslint', {}, { searchPlaces: ['tslint.json'] }),
-    format: config => JSON.stringify(config, null, '  '),
+    format: config => formatJson(config),
   },
 ];
