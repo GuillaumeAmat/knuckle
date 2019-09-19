b2a816e4f90f49ab1fa08941f37cebca0b4bfbe4
Guillaume AMAT <guillaume@amat.io>
2019-09-19 17:54:08 +0200

    test: add a test for usage print

---
+++ tests/usage.test.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { EOL } = require('os');
+
+const cmd = require('../helpers/cmd');
+
+const binFile = path.join(__dirname, '../src/bin/index.js');
+
+describe('Usage', () => {
+  it('Should print the usage', async () => {
+    const command = cmd.create(process.env.npm_node_execpath);
+
+    let response = await command.execute([binFile]);
+
+    const splittedResponse = response.trim().split(EOL + EOL);
+
+    expect(splittedResponse[0]).toMatch(/^Usage:/);
+    expect(splittedResponse[1]).toMatch(/^Options:/);
+    expect(splittedResponse[2]).toMatch(/^Commands:/);
+  });
+});
